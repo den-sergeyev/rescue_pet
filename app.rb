b948d5e8a41f2d6d21553bb6da59b383c8659a59
Den Sergeyev <dzianis_siarheyeu@epam.com>
2014-07-06 03:00:07 +0300

    Adding bootstrap.

--- app.rb
+++ app.rb
@@ -21,6 +21,14 @@ get "/" do
   haml :index
 end
 
+get "/about" do
+  haml :about
+end
+
+get "/contact" do
+  haml :contact
+end
+
 get "/pets" do
   @pets = Pet.all
   haml :'pet/index'
@@ -39,19 +47,34 @@ end
 post "/pets" do
   @pet = Pet.create(params[:pet])
   if @pet.save
-    redirect :"/pets/#{@pet.id}", :notice => "Pet was created successfully!"
+    redirect :"/pets/#{@pet.id}", :info => "Pet was created successfully!"
    else
-     haml :"pet/new", :error => "Something went wrong :("
+     haml :"pet/new", :danger => "Something went wrong :("
   end
 end
 
 get "/pets/:id/edit" do
   @pet = Pet.find(params[:id])
   haml :"pet/update"
+
 end
 
 put "/pets/:id" do
   @pet = Pet.find(params[:id])
   @pet.update(params[:pet])
   redirect :"/pets/#{@pet.id}"
+end
+
+module Sinatra
+  module Flash
+    module Style
+      def styled_flash(key=:flash)
+        return "" if flash(key).empty?
+        id = (key == :flash ? "flash" : "flash_#{key}")
+        close = '<a class="close" data-dismiss="alert" href="#">×</a>'
+        messages = flash(key).collect {|message| "  <div class='alert alert-#{message[0]}'>#{close}\n #{message[1]}</div>\n"}
+        "<div id='#{id}'>\n" + messages.join + "</div>"
+      end
+    end
+  end
 end
